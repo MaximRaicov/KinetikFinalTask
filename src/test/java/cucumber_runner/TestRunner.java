49f1b1ba6d23a936bc6f6e90ab37468567ad8cce
am-bee73 <53939796+am-bee73@users.noreply.github.com>
2019-12-13 14:01:07 +0200

    fix on test runner

--- src/test/java/cucumber_runner/TestRunner.java
+++ src/test/java/cucumber_runner/TestRunner.java
@@ -5,22 +5,15 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+/**
+ * before running any test, need to be removed #not before used tag
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = { "stepDefinition", "hooks" },
         features = { "src/test/resources/features" },
-        tags = {
-                //"@amiordan"
-                //"@mraicov"
-                //"@aciuvaga"
-        },
-        plugin = { "pretty", "html:target/cucumber" }
-
+        tags = { "not @amiordan", "not @mraicov", "not @aciuvaga" },
+        plugin = { "pretty", "html:target/cucumber" })
 public class TestRunner {
 
 }
-
-
-
-
-
